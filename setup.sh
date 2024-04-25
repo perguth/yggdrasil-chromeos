1057657e716e809f2aa00ce21ed0a3e0d511a1b4
Per Guth <mail@perguth.de>
2024-04-26 01:30:06 +0200

    Remove bugfix

--- setup.sh
+++ setup.sh
@@ -4,9 +4,6 @@ NAME=chromeos-yggdrasil-network-setup
 
 # Clean up
 trap "cd .. && rm -rf $NAME" EXIT
-  
-# Temporary bugfix as of `02/28/2020`
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 78BD65473CB3BD13
 
 # Install Ansible
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
